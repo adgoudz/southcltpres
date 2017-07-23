cafbded487cd987f867a82bd834ba87020f4b05a
Andrew Goudzwaard <adgoudz@gmail.com>
2017-07-23 20:59:21 +0000

    upgrade aldryn-django 1.8.18.1 to 1.9.13.1

--- addons/aldryn-django/aldryn_config.py
+++ addons/aldryn-django/aldryn_config.py
@@ -355,7 +355,7 @@ class Form(forms.BaseForm):
                     'stream': sys.stdout,
                 },
                 'null': {
-                    'class': 'django.utils.log.NullHandler',
+                    'class': 'logging.NullHandler',
                 },
             },
             'loggers': {
