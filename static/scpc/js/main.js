1c9433b7b7e3b38c6152a937a69a4d381bbf1354
Andrew Goudzwaard <adgoudz@gmail.com>
2017-09-24 22:51:02 -0400

    add main hero image with focus behavior

--- static/scpc/js/main.js
+++ static/scpc/js/main.js
@@ -1,6 +1,16 @@
 
 'use strict';
 
+import '../scss/main.scss';
+
 import 'modules/location';
 
-import '../scss/main.scss';
+import focusBackground from 'utils/focus';
+
+
+const defaultBackgroundWidth = 1366;
+const defaultBackgroundHeight = 911;
+
+$(window).resize(() => focusBackground($('#header'), defaultBackgroundWidth, defaultBackgroundHeight));
+
+
