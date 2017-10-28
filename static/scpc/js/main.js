cd4a645c5351ffdb0a466385769fabc05a0b97aa
Andrew Goudzwaard <adgoudz@gmail.com>
2017-10-28 13:25:14 -0400

    remove event handlers for home page alerts

--- static/scpc/js/main.js
+++ static/scpc/js/main.js
@@ -13,6 +13,3 @@ const defaultBackgroundHeight = 1267;
 
 // Focus hero on resize
 $(window).resize(() => focusBackground($('#hero'), defaultBackgroundWidth, defaultBackgroundHeight));
-
-// Hide alerts on close
-$('.close').click((e) => { $(e.target).closest('.notice').slideUp() });
